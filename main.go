cd68205ab89f51b1b4409b53d864c71623d045aa
cybercdh <cdh.cert@gmail.com>
2020-05-12 19:20:27 +0100

    added color output

--- main.go
+++ main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-	"io"
-	"net/http"
-	"net"
-	"crypto/tls"
-	"fmt"
-	"flag"
-	"strings"
-	"os"
 	"bufio"
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/gookit/color"
+	"io"
+	"net"
+	"net/http"
+	"os"
+	"strings"
 	"sync"
 	"time"
-	"github.com/PuerkitoBio/goquery"
 )
 
 var wg sync.WaitGroup
@@ -51,43 +52,49 @@ func main() {
 	}
 
 	for i := 0; i < concurrency; i++ {
-	    wg.Add(1)
+		wg.Add(1)
 
-	    go func() {
-	      for url := range urls {
+		go func() {
+			for url := range urls {
 
-	      	req, err := http.NewRequest("GET", url, nil)
-	      	if err != nil {
-	      		continue
-	      	}
+				req, err := http.NewRequest("GET", url, nil)
+				if err != nil {
+					continue
+				}
 
-	      	req.Header.Add("Connection", "close")
-	      	req.Close = true
+				req.Header.Add("Connection", "close")
+				req.Close = true
 
-	      	resp, err := client.Do(req)
+				resp, err := client.Do(req)
 
-	        if err != nil {
-	        	continue
-	        }
-	        defer resp.Body.Close()
+				if err != nil {
+					continue
+				}
+				defer resp.Body.Close()
 
-	        doc, err := goquery.NewDocumentFromReader(resp.Body)
-	        if err != nil {
-	        	continue
-	        }
+				doc, err := goquery.NewDocumentFromReader(resp.Body)
+				if err != nil {
+					continue
+				}
 
-	        title := doc.Find("title").Text()
-	        if title != "" {
+				title := doc.Find("title").Text()
+				if title != "" {
+					if strings.Contains(title, "403") {
+						color.Yellow.Printf("%s : %s\n", url, title)
+					} else if strings.Contains(title, "30") {
+						color.Cyan.Printf("%s : %s\n", url, title)
+					} else {
 						fmt.Printf("%s : %s\n", url, title)
-	        } else {
-	        	if !ignoreblanks {
-		        	fmt.Printf("%s : No title\n", url)
-	        	}
-	      	}
+					}
+				} else {
+					if !ignoreblanks {
+						fmt.Printf("%s : No title\n", url)
+					}
+				}
 
-	      }
-	      wg.Done()
-	    }()
+			}
+			wg.Done()
+		}()
 	}
 
 	var input_urls io.Reader
@@ -95,20 +102,20 @@ func main() {
 
 	arg_url := flag.Arg(0)
 	if arg_url != "" {
-	    input_urls = strings.NewReader(arg_url)
+		input_urls = strings.NewReader(arg_url)
 	}
 
 	sc := bufio.NewScanner(input_urls)
-	
+
 	for sc.Scan() {
-			url := sc.Text()
-			if strings.HasPrefix(url, "http") {
-				urls <- url
-			} else {
-				urls <- "http://" + url
-				urls <- "https://" + url
-			}
-	    
+		url := sc.Text()
+		if strings.HasPrefix(url, "http") {
+			urls <- url
+		} else {
+			urls <- "http://" + url
+			urls <- "https://" + url
+		}
+
 	}
 
 	close(urls)
